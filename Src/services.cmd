a0415edb65cd87cb526e4f1306b83557b366001f
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-08-24 11:40:24 +0200

    Update configs

---
+++ Src/services.cmd
@@ -0,0 +1,216 @@
+﻿sc config "StiSvc" start= auto
+sc start "StiSvc"
+sc config "COMSysApp" start= auto
+sc start "COMSysApp"
+sc config "lmhosts" start= auto
+sc start "lmhosts"
+sc config "SCardSvr" start= auto
+sc start "SCardSvr"
+sc config "SessionEnv" start= auto
+sc start "SessionEnv"
+sc config "DusmSvc" start= auto
+sc start "DusmSvc"
+sc config "WpcMonSvc" start= auto
+sc start "WpcMonSvc"
+sc config "dcsvc" start= auto
+sc start "dcsvc"
+sc config "SSDPSRV" start= auto
+sc start "SSDPSRV"
+sc config "WiaRpc" start= auto
+sc start "WiaRpc"
+sc config "QWAVE" start= auto
+sc start "QWAVE"
+sc config "DiagTrack" start= auto
+sc start "DiagTrack"
+sc config "PrintNotify" start= auto
+sc start "PrintNotify"
+sc config "WinRM" start= auto
+sc start "WinRM"
+sc config "XblAuthManager" start= auto
+sc start "XblAuthManager"
+sc config "MapsBroker" start= auto
+sc start "MapsBroker"
+sc config "RasAuto" start= auto
+sc start "RasAuto"
+sc config "RasMan" start= auto
+sc start "RasMan"
+sc config "SEMgrSvc" start= auto
+sc start "SEMgrSvc"
+sc config "GraphicsPerfSvc" start= auto
+sc start "GraphicsPerfSvc"
+sc config "p2psvc" start= auto
+sc start "p2psvc"
+sc config "autotimesvc" start= auto
+sc start "autotimesvc"
+sc config "PerfHost" start= auto
+sc start "PerfHost"
+sc config "upnphost" start= auto
+sc start "upnphost"
+sc config "InstallService" start= auto
+sc start "InstallService"
+sc config "ibtsiva" start= auto
+sc start "ibtsiva"
+sc config "jhi_service" start= auto
+sc start "jhi_service"
+sc config "LMS" start= auto
+sc start "LMS"
+sc config "EvtEng" start= auto
+sc start "EvtEng"
+sc config "RegSrvc" start= auto
+sc start "RegSrvc"
+sc config "ZeroConfigService" start= auto
+sc start "ZeroConfigService"
+sc config "IAStorDataMgrSvc" start= auto
+sc start "IAStorDataMgrSvc"
+sc config "HfcDisableService" start= auto
+sc start "HfcDisableService"
+sc config "Intel(R) TPM Provisioning Service" start= auto
+sc start "Intel(R) TPM Provisioning Service"
+sc config "RpcLocator" start= auto
+sc start "RpcLocator"
+sc config "edgeupdate" start= auto
+sc start "edgeupdate"
+sc config "edgeupdatem" start= auto
+sc start "edgeupdatem"
+sc config "uhssvc" start= auto
+sc start "uhssvc"
+sc config "FrameServerMonitor" start= auto
+sc start "FrameServerMonitor"
+sc config "SharedAccess" start= auto
+sc start "SharedAccess"
+sc config "PlugPlay" start= auto
+sc start "PlugPlay"
+sc config "tzautoupdate" start= auto
+sc start "tzautoupdate"
+sc config "PNRPsvc" start= auto
+sc start "PNRPsvc"
+sc config "RtkAudioUniversalService" start= auto
+sc start "RtkAudioUniversalService"
+sc config "RemoteRegistry" start= auto
+sc start "RemoteRegistry"
+sc config "vmicvss" start= auto
+sc start "vmicvss"
+sc config "RemoteAccess" start= auto
+sc start "RemoteAccess"
+sc config "RstMwService" start= auto
+sc start "RstMwService"
+sc config "LanmanServer" start= auto
+sc start "LanmanServer"
+sc config "FrameServer" start= auto
+sc start "FrameServer"
+sc config "LicenseManager" start= auto
+sc start "LicenseManager"
+sc config "vmicshutdown" start= auto
+sc start "vmicshutdown"
+sc config "BthAvctpSvc" start= auto
+sc start "BthAvctpSvc"
+sc config "InventorySvc" start= auto
+sc start "InventorySvc"
+sc config "fhsvc" start= auto
+sc start "fhsvc"
+sc config "DisplayEnhancementService" start= auto
+sc start "DisplayEnhancementService"
+sc config "WbioSrvc" start= auto
+sc start "WbioSrvc"
+sc config "FontCache" start= auto
+sc start "FontCache"
+sc config "BDESVC" start= auto
+sc start "BDESVC"
+sc config "WlanSvc" start= auto
+sc start "WlanSvc"
+sc config "SharedRealitySvc" start= auto
+sc start "SharedRealitySvc"
+sc config "lfsvc" start= auto
+sc start "lfsvc"
+sc config "RmSvc" start= auto
+sc start "RmSvc"
+sc config "PcaSvc" start= auto
+sc start "PcaSvc"
+sc config "wbengine" start= auto
+sc start "wbengine"
+sc config "NetTcpPortSharing" start= auto
+sc start "NetTcpPortSharing"
+sc config "DsSvc" start= auto
+sc start "DsSvc"
+sc config "BTAGService" start= auto
+sc start "BTAGService"
+sc config "CDPSvc" start= auto
+sc start "CDPSvc"
+sc config "AJRouter" start= auto
+sc start "AJRouter"
+sc config "StorSvc" start= auto
+sc start "StorSvc"
+sc config "SensrSvc" start= auto
+sc start "SensrSvc"
+sc config "vmicrdv" start= auto
+sc start "vmicrdv"
+sc config "LxpSvc" start= auto
+sc start "LxpSvc"
+sc config "HvHost" start= auto
+sc start "HvHost"
+sc config "SensorDataService" start= auto
+sc start "SensorDataService"
+sc config "WpnService" start= auto
+sc start "WpnService"
+sc config "vmickvpexchange" start= auto
+sc start "vmickvpexchange"
+sc config "WPDBusEnum" start= auto
+sc start "WPDBusEnum"
+sc config "WdiServiceHost" start= auto
+sc start "WdiServiceHost"
+sc config "vmicvmsession" start= auto
+sc start "vmicvmsession"
+sc config "MSiSCSI" start= auto
+sc start "MSiSCSI"
+sc config "icssvc" start= auto
+sc start "icssvc"
+sc config "vmicheartbeat" start= auto
+sc start "vmicheartbeat"
+sc config "PushToInstall" start= auto
+sc start "PushToInstall"
+sc config "SmsRouter" start= auto
+sc start "SmsRouter"
+sc config "vmictimesync" start= auto
+sc start "vmictimesync"
+sc config "PhoneSvc" start= auto
+sc start "PhoneSvc"
+sc config "WFDSConMgrSvc" start= auto
+sc start "WFDSConMgrSvc"
+sc config "wisvc" start= auto
+sc start "wisvc"
+sc config "MixedRealityOpenXRSvc" start= auto
+sc start "MixedRealityOpenXRSvc"
+sc config "SstpSvc" start= auto
+sc start "SstpSvc"
+sc config "TermService" start= auto
+sc start "TermService"
+sc config "shpamsvc" start= auto
+sc start "shpamsvc"
+sc config "smphost" start= auto
+sc start "smphost"
+sc config "Spooler" start= auto
+sc start "Spooler"
+sc config "LanmanWorkstation" start= auto
+sc start "LanmanWorkstation"
+sc config "EventSystem" start= auto
+sc start "EventSystem"
+sc config "TapiSrv" start= auto
+sc start "TapiSrv"
+sc config "W32Time" start= auto
+sc start "W32Time"
+sc config "Themes" start= auto
+sc start "Themes"
+sc config "UWPService" start= auto
+sc start "UWPService"
+sc config "WalletService" start= auto
+sc start "WalletService"
+sc config "WarpJITSvc" start= auto
+sc start "WarpJITSvc"
+sc config "WSearch" start= auto
+sc start "WSearch"
+sc config "MyWiFiDHCPDNS" start= auto
+sc start "MyWiFiDHCPDNS"
+sc config "XboxGipSvc" start= auto
+sc start "XboxGipSvc"
+sc config "NVDisplay.ContainerLocalSystem" start= auto
+sc start "NVDisplay.ContainerLocalSystem"
