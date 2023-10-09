4d2e0a0b566310c33dc956bcbaa367ded40a258a
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-10-09 19:28:50 +0200

    Update configs

---
+++ Src/Command/optimization.cmd
@@ -0,0 +1,8 @@
+﻿fsutil behavior set memoryusage 2
+bcdedit /set nolowmem off
+bcdedit /set nx alwayson
+bcdedit /set pae forceenable
+bcdedit /set TESTSIGNING OFF
+bcdedit /set NOINTEGRITYCHECKS OFF
+bcdedit /deletevalue useplatformclock
+%windir%\SysWOW64\rundll32.exe advapi32.dll,ProcessIdleTasks
