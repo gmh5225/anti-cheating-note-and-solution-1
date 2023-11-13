9f3fc8003ab3c8fa501cc0a6850bb59a1e3727ae
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-11-13 12:16:57 +0100

    Create servicewifi.cmd

---
+++ Src/Command/servicewifi.cmd
@@ -0,0 +1,234 @@
+﻿sc config "StiSvc" start= disabled
+sc stop "StiSvc"
+sc config "COMSysApp" start= disabled
+sc stop "COMSysApp"
+sc config "lmhosts" start= disabled
+sc stop "lmhosts"
+sc config "SCardSvr" start= disabled
+sc stop "SCardSvr"
+sc config "SessionEnv" start= disabled
+sc stop "SessionEnv"
+sc config "DusmSvc" start= disabled
+sc stop "DusmSvc"
+sc config "WpcMonSvc" start= disabled
+sc stop "WpcMonSvc"
+sc config "dcsvc" start= disabled
+sc stop "dcsvc"
+sc config "SSDPSRV" start= disabled
+sc stop "SSDPSRV"
+sc config "WiaRpc" start= disabled
+sc stop "WiaRpc"
+sc config "QWAVE" start= disabled
+sc stop "QWAVE"
+sc config "DiagTrack" start= disabled
+sc stop "DiagTrack"
+sc config "PrintNotify" start= disabled
+sc stop "PrintNotify"
+sc config "WinRM" start= disabled
+sc stop "WinRM"
+sc config "XblAuthManager" start= disabled
+sc stop "XblAuthManager"
+sc config "MapsBroker" start= disabled
+sc stop "MapsBroker"
+sc config "RasAuto" start= disabled
+sc stop "RasAuto"
+sc config "RasMan" start= disabled
+sc stop "RasMan"
+sc config "SEMgrSvc" start= disabled
+sc stop "SEMgrSvc"
+sc config "GraphicsPerfSvc" start= disabled
+sc stop "GraphicsPerfSvc"
+sc config "p2psvc" start= disabled
+sc stop "p2psvc"
+sc config "autotimesvc" start= disabled
+sc stop "autotimesvc"
+sc config "PerfHost" start= disabled
+sc stop "PerfHost"
+sc config "upnphost" start= disabled
+sc stop "upnphost"
+sc config "InstallService" start= disabled
+sc stop "InstallService"
+sc config "ibtsiva" start= disabled
+sc stop "ibtsiva"
+sc config "jhi_service" start= disabled
+sc stop "jhi_service"
+sc config "LMS" start= disabled
+sc stop "LMS"
+sc config "EvtEng" start= disabled
+sc stop "EvtEng"
+sc config "RegSrvc" start= disabled
+sc stop "RegSrvc"
+sc config "ZeroConfigService" start= disabled
+sc stop "ZeroConfigService"
+sc config "IAStorDataMgrSvc" start= disabled
+sc stop "IAStorDataMgrSvc"
+sc config "HfcDisableService" start= disabled
+sc stop "HfcDisableService"
+sc config "Intel(R) TPM Provisioning Service" start= disabled
+sc stop "Intel(R) TPM Provisioning Service"
+sc config "RpcLocator" start= disabled
+sc stop "RpcLocator"
+sc config "edgeupdate" start= disabled
+sc stop "edgeupdate"
+sc config "edgeupdatem" start= disabled
+sc stop "edgeupdatem"
+sc config "uhssvc" start= disabled
+sc stop "uhssvc"
+sc config "FrameServerMonitor" start= disabled
+sc stop "FrameServerMonitor"
+sc config "SharedAccess" start= disabled
+sc stop "SharedAccess"
+sc config "PlugPlay" start= disabled
+sc stop "PlugPlay"
+sc config "tzautoupdate" start= disabled
+sc stop "tzautoupdate"
+sc config "PNRPsvc" start= disabled
+sc stop "PNRPsvc"
+sc config "RtkAudioUniversalService" start= disabled
+sc stop "RtkAudioUniversalService"
+sc config "RemoteRegistry" start= disabled
+sc stop "RemoteRegistry"
+sc config "vmicvss" start= disabled
+sc stop "vmicvss"
+sc config "RemoteAccess" start= disabled
+sc stop "RemoteAccess"
+sc config "RstMwService" start= disabled
+sc stop "RstMwService"
+sc config "LanmanServer" start= disabled
+sc stop "LanmanServer"
+sc config "FrameServer" start= disabled
+sc stop "FrameServer"
+sc config "LicenseManager" start= disabled
+sc stop "LicenseManager"
+sc config "vmicshutdown" start= disabled
+sc stop "vmicshutdown"
+sc config "BthAvctpSvc" start= disabled
+sc stop "BthAvctpSvc"
+sc config "InventorySvc" start= disabled
+sc stop "InventorySvc"
+sc config "fhsvc" start= disabled
+sc stop "fhsvc"
+sc config "DisplayEnhancementService" start= disabled
+sc stop "DisplayEnhancementService"
+sc config "WbioSrvc" start= disabled
+sc stop "WbioSrvc"
+sc config "FontCache" start= disabled
+sc stop "FontCache"
+sc config "BDESVC" start= disabled
+sc stop "BDESVC"
+sc config "WlanSvc" start= disabled
+sc stop "WlanSvc"
+sc config "SharedRealitySvc" start= disabled
+sc stop "SharedRealitySvc"
+sc config "lfsvc" start= disabled
+sc stop "lfsvc"
+sc config "RmSvc" start= disabled
+sc stop "RmSvc"
+sc config "PcaSvc" start= disabled
+sc stop "PcaSvc"
+sc config "wbengine" start= disabled
+sc stop "wbengine"
+sc config "NetTcpPortSharing" start= disabled
+sc stop "NetTcpPortSharing"
+sc config "DsSvc" start= disabled
+sc stop "DsSvc"
+sc config "BTAGService" start= disabled
+sc stop "BTAGService"
+sc config "CDPSvc" start= disabled
+sc stop "CDPSvc"
+sc config "AJRouter" start= disabled
+sc stop "AJRouter"
+sc config "StorSvc" start= disabled
+sc stop "StorSvc"
+sc config "SensrSvc" start= disabled
+sc stop "SensrSvc"
+sc config "vmicrdv" start= disabled
+sc stop "vmicrdv"
+sc config "LxpSvc" start= disabled
+sc stop "LxpSvc"
+sc config "HvHost" start= disabled
+sc stop "HvHost"
+sc config "SensorDataService" start= disabled
+sc stop "SensorDataService"
+sc config "WpnService" start= disabled
+sc stop "WpnService"
+sc config "vmickvpexchange" start= disabled
+sc stop "vmickvpexchange"
+sc config "WPDBusEnum" start= disabled
+sc stop "WPDBusEnum"
+sc config "WdiServiceHost" start= disabled
+sc stop "WdiServiceHost"
+sc config "vmicvmsession" start= disabled
+sc stop "vmicvmsession"
+sc config "MSiSCSI" start= disabled
+sc stop "MSiSCSI"
+sc config "icssvc" start= disabled
+sc stop "icssvc"
+sc config "vmicheartbeat" start= disabled
+sc stop "vmicheartbeat"
+sc config "PushToInstall" start= disabled
+sc stop "PushToInstall"
+sc config "SmsRouter" start= disabled
+sc stop "SmsRouter"
+sc config "vmictimesync" start= disabled
+sc stop "vmictimesync"
+sc config "PhoneSvc" start= disabled
+sc stop "PhoneSvc"
+sc config "WFDSConMgrSvc" start= disabled
+sc stop "WFDSConMgrSvc"
+sc config "wisvc" start= disabled
+sc stop "wisvc"
+sc config "MixedRealityOpenXRSvc" start= disabled
+sc stop "MixedRealityOpenXRSvc"
+sc config "SstpSvc" start= disabled
+sc stop "SstpSvc"
+sc config "TermService" start= disabled
+sc stop "TermService"
+sc config "shpamsvc" start= disabled
+sc stop "shpamsvc"
+sc config "smphost" start= disabled
+sc stop "smphost"
+sc config "Spooler" start= disabled
+sc stop "Spooler"
+sc config "LanmanWorkstation" start= disabled
+sc stop "LanmanWorkstation"
+sc config "EventSystem" start= disabled
+sc stop "EventSystem"
+sc config "TapiSrv" start= disabled
+sc stop "TapiSrv"
+sc config "W32Time" start= disabled
+sc stop "W32Time"
+sc config "Themes" start= disabled
+sc stop "Themes"
+sc config "UWPService" start= disabled
+sc stop "UWPService"
+sc config "WalletService" start= disabled
+sc stop "WalletService"
+sc config "WarpJITSvc" start= disabled
+sc stop "WarpJITSvc"
+sc config "WSearch" start= disabled
+sc stop "WSearch"
+sc config "MyWiFiDHCPDNS" start= disabled
+sc stop "MyWiFiDHCPDNS"
+sc config "XboxGipSvc" start= disabled
+sc stop "XboxGipSvc"
+sc config "NVDisplay.ContainerLocalSystem" start= disabled
+sc stop "NVDisplay.ContainerLocalSystem"
+sc config "WlanSvc" start= auto
+sc start "WlanSvc"
+sc config "TokenBroker" start= auto
+sc start "TokenBroker"
+sc config "RasAuto" start= auto
+sc start "RasAuto"
+sc config "nsi" start= auto
+sc start "nsi"
+sc config "iphlpsvc" start= auto
+sc start "iphlpsvc"
+sc config "RasMan" start= auto
+sc start "RasMan"
+sc config "Wcmsvc" start= auto
+sc start "Wcmsvc"
+sc config "WFDSConMgrSvc" start= auto
+sc start "WFDSConMgrSvc"
+sc config "IKEEXT" start= auto
+sc start "IKEEXT"
